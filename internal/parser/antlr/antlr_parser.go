75007a7ea31d3690b0e9b68765ad2fbc02e68bb1
Sidhant Aggarwal <10743214+sidhant92@users.noreply.github.com>
2023-02-12 23:45:35 +0530

    add caching support

--- internal/parser/antlr/antlr_parser.go
+++ internal/parser/antlr/antlr_parser.go
@@ -5,13 +5,31 @@ import (
 	"bool-parser-go/pkg/domain"
 	"errors"
 	"github.com/antlr/antlr4/runtime/Go/antlr/v4"
+	lru "github.com/hashicorp/golang-lru/v2"
 	"log"
 )
 
 type ANTLRParser struct {
+	UseCache bool
+	cache    *lru.Cache[string, domain.Node]
 }
 
-func (p ANTLRParser) Parse(input string) (res domain.Node, err error) {
+func Default() *ANTLRParser {
+	return &ANTLRParser{
+		UseCache: false,
+		cache:    nil,
+	}
+}
+
+func Cached(size int) *ANTLRParser {
+	cache, _ := lru.New[string, domain.Node](size)
+	return &ANTLRParser{
+		UseCache: true,
+		cache:    cache,
+	}
+}
+
+func (p *ANTLRParser) Parse(input string) (res domain.Node, err error) {
 	defer func() {
 		if r := recover(); r != nil {
 			log.Println("panic occurred:", r)
@@ -22,10 +40,17 @@ func (p ANTLRParser) Parse(input string) (res domain.Node, err error) {
 			case error:
 				err = x
 			default:
-				err = errors.New("Unknown panic")
+				err = errors.New("unknown panic")
 			}
 		}
 	}()
+
+	if p.UseCache {
+		val, ok := p.cache.Get(input)
+		if ok {
+			return val, nil
+		}
+	}
 
 	inputStream := antlr.NewInputStream(input)
 	lexer := lib.NewBooleanExpressionLexer(inputStream)
@@ -33,5 +58,8 @@ func (p ANTLRParser) Parse(input string) (res domain.Node, err error) {
 	antlrParser := lib.NewBooleanExpressionParser(stream)
 	listener := New()
 	antlr.ParseTreeWalkerDefault.Walk(listener, antlrParser.Parse())
+	if p.UseCache {
+		p.cache.Add(input, listener.GetResult())
+	}
 	return listener.GetResult(), nil
 }
